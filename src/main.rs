c5ab7efd6856201662fc7920f9bcfc860c281197
Ryan Peach <ryan.peach@keysight.com>
2019-11-12 16:53:22 -0500

    Made the creation of sensor_x and sensor_y more elegant

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate image;
 extern crate cgmath;
 
-use cgmath::{Point3, Vector3};
+use cgmath::{Point3, Vector3, InnerSpace};
 use image::{DynamicImage, GenericImageView};
 
 // REF: https://bheisler.github.io/post/writing-raytracer-in-rust-part-1/
@@ -57,9 +57,28 @@ pub struct Ray {
 // Prime rays are those that come from the camera, traced through the pixel, into the scene
 impl Ray {
     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
+
+        // This describes how the ray direction is calculated
+        // First the pixel center is calculated as it's starting value + half a pixel
+        // Then it's normalized to the width of the scene
+        // Then it's adjusted from coordinates (0..1) to (-1..1) via *2
+        fn sensor(scene: &Scene, v: u32) -> f64 {
+            let pixel_center = v as f64 + 0.5;
+            let normalized_to_width = pixel_center / scene.width as f64;
+            let adjusted_screen_pos = (normalized_to_width * 2.0) - 1.0;
+            adjusted_screen_pos
+        }
+
+        let sensor_x =  sensor(scene, x);
+        let sensor_y = -sensor(scene, y); // y is positive in the down direction
+
         Ray {
             origin: Point3::new(0.0, 0.0, 0.0),
-            direction: Vector3::new(0.0, 0.0, 0.0),
+            direction: Vector3 {
+                x: sensor_x,
+                y: sensor_y,
+                z: -1.0
+            }.normalize()
         }
     }
 }
